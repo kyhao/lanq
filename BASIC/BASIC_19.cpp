4b001d3c07185b4dc3913853e43343df56e5f316
kyhao <925579748@qq.com>
2018-01-30 20:55:04 +0800

    update

--- BASIC/BASIC_19.cpp
+++ BASIC/BASIC_19.cpp
@@ -1,4 +1,4 @@
-//采用递归法，暴力枚举，需优化 
+//BASIC_19 优化后题型 
 #include <iostream>
 #include <string>
 using namespace std;
@@ -10,36 +10,45 @@ bool ishuiwen(const string& str){
 	return true;
 }
 
-void f(string str, int x, int times){
-	
-	//...
-	char temp;
-	for(int i = x, c = 0; i < str.length(); i++, c++){
-		
-		if( x == str.length() )return;
-		if( ishuiwen(str) ){
-			cout << str << "-->";
-			cout << times << "\n";
-			return;
-		}
-		
-		temp = str[i];
-		str.erase(i, 1);
-		str.insert(x, 1, temp);
-						
-		f(str, x + 1, times + c);
+//输出所需的次数 str：输入字符串 lon：长度 x：起始匹配位置 times：移动次数 
+void f(string &str, const int &lon, int x, int times){
+	//若 x 到达中点或者已经是回型数时停止 
+	if( x == lon/2 | ishuiwen(str) ){
+		//cout << str << " ->> ";
+		cout << times;
+		return;
 	}
-	//...
-	
+	char temp; int c = 0;	//temp：临时存储变量  c: 每一次的步骤和 
+	for(int i = lon - 1 - x; i > x; i--){//从后向前寻找相同元素，移至对称位置 
+		if( str[x] == str[i] ){
+			temp = str[i];
+			str.erase(i, 1);
+			str.insert( lon - 1 - x, 1, temp);
+			//cout << str << " >> " << c <<"\n";
+			break;
+		}
+		c++;	//每一次步骤加一 
+	}
+	//若未寻找到相同元素，则该元素为单，若字符串为偶数则一定不成立。
+	//若为奇数，则该数一定位于中心，取该数后的串进行移动。可以判别双‘成单数’。 
+	if( str[x] != str[lon - 1 -x] ){
+		if( lon % 2 == 0 ){
+			cout << "Impossible"; return;
+		}
+		c = lon/2 - x;
+		string str1 = str.substr(x + 1, lon - x*2 - 1 );	//取该成单数 的后面字符串 
+		f(str1, lon - x*2 - 1, 0, times + c);
+	}else{
+		f(str, lon, x + 1, times + c);
+	}		
 }
 
 int main(){
-	//int n;
+	
+	int n;
 	string str;
-	//cin >> n >> str;
-	cin >> str; 
-	//cout << ishuiwen(str);
-	f(str, 0, 0);
+	cin >> n >> str;
+	f(str, n, 0, 0);
 	
 	return 0;
 }
