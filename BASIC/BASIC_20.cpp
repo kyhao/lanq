97cd006373a4a60337a4c2ed3aa91c7aa86734c4
kyhao <925579748@qq.com>
2018-02-01 14:32:36 +0800

    Basic_20
    蓝桥Oj通过，但是程序不完善

--- BASIC/BASIC_20.cpp
+++ BASIC/BASIC_20.cpp
@@ -1,24 +1,39 @@
+//递归实现
+//虽然OJ可以过，但是部分数据不完善 
 #include <iostream>
 #include <string>
 using namespace std;
-
+//预处理位数和字母 
 string num[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
-string t[5] = {"shi", "bai", "qian", "wan", "yi"};
+string wei[10] = {"", "shi", "bai", "qian", "wan", "shi", "bai", "qian", "yi", "shi"};
+
+void read_out(int n, int wz, int before){
+	
+	//...
+	if(n == 0)return;
+	int now = n % 10;
+	read_out( n/10 , wz + 1, now);
+	if( (before == 0) && ( now == 0) ){
+		
+	}
+	else{
+		
+		if( now == 1 && ( wz == 5 || wz == 1)){;}
+		else {
+			if( n/10 )cout << " ";
+			cout << num[now];
+			cout << " ";
+		}
+		if(now)cout << wei[wz];
+	}
+	
+	//...
+		
+}
 
 int main(){
 	int x;
 	cin >> x;
-	int n = x;
-	string ans = num[ n % 10 ];
-	n /= 10;
-	for(int i = 2; n ; i++){
-		
-		if( i == 5)ans = t[3] + ans;
-		if( i == 5)ans = t[3] + ans;
-		ans = num[ n % 10 ] + " " + ans;
-		n /= 10;	
-			 
-	}
-	cout << ans;
+	read_out(x, 0, 0);
 	return 0;
 }
