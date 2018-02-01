fa23757141259fb5c93851b14e7e71009848770f
kyhao <925579748@qq.com>
2018-02-01 14:32:46 +0800

    update

---
+++ BASIC/BASIC_23.cpp
@@ -0,0 +1,18 @@
+//–æ∆¨≤‚ ‘ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int** m = new int*[n];
+	for( int i = 0; i < n; i++){
+		m[i] = new int[n];
+		for( int j = 0; j < n; j++)
+			cin >> m[i][j];
+	}
+	
+	
+	
+	return 0;
+} 
