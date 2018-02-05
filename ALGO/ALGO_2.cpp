a12a25d88747e572058f0b960d7b3f49563d28d8
kyhao <925579748@qq.com>
2018-02-05 18:15:45 +0800

    update

--- ALGO/ALGO_2.cpp
+++ ALGO/ALGO_2.cpp
@@ -2,14 +2,16 @@
 #include <vector>
 using namespace std;
 
+#define BASE 1000 
+
 class b_int{
 	
 public:	
 	b_int(int n = 0){
 		num.clear();
 		for(;n;){
-			num.push_back( n % 1000 );
-			n /= 1000;
+			num.push_back( n % BASE );
+			n /= BASE;
 		}		
 	}
 	
@@ -19,12 +21,12 @@ public:
 		int t;
 		for(int i = 0; i < this->num.size(); i++){
 			t = num[i] * n + c;
-			ans.num.push_back( t % 1000 );
-			c = t / 1000;
+			ans.num.push_back( t % BASE );
+			c = t / BASE;
 		}
 		while(c){
-			ans.num.push_back( c % 1000);
-			c /= 1000;
+			ans.num.push_back( c % BASE);
+			c /= BASE;
 		}
 		return ans;
 	}
@@ -36,13 +38,11 @@ int main(){
 	int n, lol;
 	b_int ans;
 	cin >> n;
-	if( ( n % 2 ) == 0 ){
-		lol = 3;
-	}else{
-		lol = 2;
-	}
+	
+	
+	
 	b_int num(n);
-	ans = num * ( n - 1 ) * ( n - lol );
+	ans = num * ( n - 1 ) * ( n - 2 );
 	for(int i = ans.num.size() - 1; i >= 0; i--){
 		if(ans.num[i] < 10)cout << "00";
 		else if(ans.num[i] < 100 )cout << "0";
--- ALGO/ALGO_3.cpp
+++ ALGO/ALGO_3.cpp
@@ -1,15 +1,39 @@
+//K好数 L位K进制 
 #include <iostream>
 using namespace std;
 
+#define BASE 1000000007 
+
+bool check(int *n, int l){
+	for(int i = 1; i < l - 1; i++){
+		if( ( n[i]-n[i-1] ) == 1 || ( n[i]-n[i-1] ) == -1 )return 0;
+	}
+	return 1;
+}
+
+// L位 K进制 
 int main(){
 	int K, L;
 	cin >> K >> L;
-	int *N = new int[L];
-	N[L - 1] = 1;
-	for(int i = 0; i < L - 1; i++){
-		N[i] = 0;
+	int count = 0;
+	
+	int* n = new int[L];
+	for(int i = 0; i < L; i++)n[i] = 0; 
+	n[ L - 1 ] = 1;
+	
+	for(int j = 0; j < L;){		
+		n[j]++;
+		if( check(n, L) ){
+			count++;
+		}
+		if(n[j] == K){
+			n[j] = 0;
+			j++;
+		}		
 	}
 	
-	cout << N[0] << N[1];
+	cout << count;
+	
+	delete n;
 	return 0;
 }
